f0501ad4397c685bb7a40f1f8800b4978a96d142
inec <luyege+github@gmail.com>
2018-02-26 22:17:56 -0600

    'test'

--- frontend/src/app/question.components.ts
+++ frontend/src/app/question.components.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'question',
-    template:'this is our question'
+    template:'this is our question <button mat-button>Click me!</button>'
 })
 export class QuestionComponent {
 
