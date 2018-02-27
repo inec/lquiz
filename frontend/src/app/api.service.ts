a7f6da6c9c8042be7061e96805f80a30ca243bb6
inec <luyege+github@gmail.com>
2018-02-26 23:07:11 -0600

    'test'

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+
+@Injectable()
 export class ApiService {
 
-    post(question){
-        console.log(question)
+    constructor(private http: HttpClient){
+
+    }
+    postQuestion(question){
+        console.log(question+'-uuu')
+        this.http.post('',question).subscribe(res=>{
+            console.log(res+'-e')
+        }        )
+     
+
     }
 
 }
