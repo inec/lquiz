aa9d92980652b202e65e93154bc169692cf57fba
inec <luyege+github@gmail.com>
2018-03-07 23:33:28 -0600

    'test'

---
+++ quiz-backend/quiz-backend/Models/Quiz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiz_backend.Models
+{
+    public class Quiz
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string Answer1 { get; set; }
+     
+    }
+}
