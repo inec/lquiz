8beed1a3d20faba13ac94aeb9b730f0f65ebd8f5
inec <luyege+github@gmail.com>
2018-03-12 23:44:54 -0500

    't'

--- quiz-backend/quiz-backend/UserDbContext.cs
+++ quiz-backend/quiz-backend/UserDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,9 @@ namespace quiz_backend
 {
     public class UserDbContext:IdentityDbContext<IdentityUser>
     {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options)
+        {
+
+        }
     }
 }
