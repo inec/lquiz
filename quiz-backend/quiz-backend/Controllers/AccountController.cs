fe9f98f2f80864a6c9c1d83b811f0fe385c70ae6
inec <luyege+github@gmail.com>
2018-03-12 23:29:17 -0500

    'actctrl'

---
+++ quiz-backend/quiz-backend/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace quiz_backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : Controller
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Re 
+    }
+}
