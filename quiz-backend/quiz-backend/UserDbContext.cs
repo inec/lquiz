0fe8a70dd7ea9c8a2e47a488fedbecfef2e06120
inec <luyege+github@gmail.com>
2018-03-12 23:42:08 -0500

    'UserDbContext.cs'

---
+++ quiz-backend/quiz-backend/UserDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiz_backend
+{
+    public class UserDbContext:IdentityDbContext<IdentityUser>
+    {
+    }
+}
