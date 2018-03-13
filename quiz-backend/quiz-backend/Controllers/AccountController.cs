b9ccaf41a584b1e1c006e3108469daf8cc31520c
inec <luyege+github@gmail.com>
2018-03-12 23:51:44 -0500

    'usermanage'

--- quiz-backend/quiz-backend/Controllers/AccountController.cs
+++ quiz-backend/quiz-backend/Controllers/AccountController.cs
@@ -5,22 +5,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 
 namespace quiz_backend.Controllers
 {
+
+    public class Credentials
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
     [Produces("application/json")]
     [Route("api/Account")]
     public class AccountController : Controller
     {
-        public class Credentials
+        readonly UserManager<IdentityUser> userManage;
+       public AccountController(UserManager<IdentityUser> userManager)
         {
-            public string Email { get; set; }
-
-            public string Password { get; set; }
-
+            this.userManage = userManage;
+        }
             [HttpPost]
         public async Task<IActionResult> Register([FromBody] Credentials credentials)
         {
+                var user = new IdentityUser { UserName = credentials.Email, Email = credentials.Email };
+
                 var jwt = new JwtSecurityToken();
                 return Ok(new JwtSecurityTokenHandler().WriteToken(jwt));
         } 
