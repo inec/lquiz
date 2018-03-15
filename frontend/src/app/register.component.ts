060ba3a8292d26810c55e3d449242316f865bf4e
inec <luyege+github@gmail.com>
2018-03-14 23:33:09 -0500

    'formGroup'

--- frontend/src/app/register.component.ts
+++ frontend/src/app/register.component.ts
@@ -11,7 +11,10 @@ import { ApiService } from './api.service';
 export class RegisterComponent {
    
     form
-    constructor(private api: ApiService,private fb: FormBuilder){}
+    constructor(private api: ApiService,private fb: FormBuilder){
+
+        
+    }
 
     ngOnInit(){
     //this.api.quizSelected.subscribe(quiz => this.quiz=quiz)
